7fceb9c81d1754df26bff2a41c6611dde1e912c9
AgentWombat <jtzingale@gmail.com>
2025-07-01 10:18:26 -0700

    reorganized web_interface routes

--- ucr_chatbot/web_interface/routes.py
+++ ucr_chatbot/web_interface/routes.py
@@ -1,4 +1,9 @@
-from flask import Blueprint, render_template, url_for
+from flask import (
+    Blueprint, 
+    render_template, 
+    url_for, 
+    redirect, # type: ignore
+    )
 
 bp = Blueprint("routes", __name__)
 
@@ -14,10 +19,18 @@ def course_selection():
 
 
 @bp.route("/chat/<int:course_id>")
-def chat(course_id: int):
+def chat(course_id: int):  # type: ignore
+    """Redirects to a page with a new conversation for a course.
+    :param course_id: The id of the course for which a conversation will be initialized.
+    """
+    return redirect(url_for(".conversation", conversation_id=course_id))  # type: ignore
+
+
+@bp.route("/chat/convsersation/<int:conversation_id>")
+def conversation(conversation_id: int):
     """Responds with page where a student can interact with a chatbot for a course with <course_id>."""
     return render_template(
         "base.html",
         title="Landing Page",
-        body=f"Chat with me about the course with id {course_id}!",
+        body=f"Chat with me about the course for which the conversation with id {conversation_id} exists.",
     )
