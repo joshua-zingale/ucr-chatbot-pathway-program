0155e8f86f7aee653b5d9cffc323c424ed3a4c72
Chris <cullm001@ucr.edu>
2025-07-09 14:01:20 -0700

    db and tests

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,38 +1,42 @@
 import sys
 import os
-sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
-from ucr_chatbot import create_app
-from flask import Flask
 import pytest
+from flask import Flask
+from flask.testing import FlaskClient
+from sqlalchemy import create_engine
+from sqlalchemy.engine import Connection
+from typing import Generator
+from ucr_chatbot import create_app
+from dotenv import load_dotenv
+import os
 
-from sqlalchemy import create_engine, String, Column, Integer, DateTime, ForeignKey, Text, insert
-from sqlalchemy.orm import declarative_base, mapped_column
-from datetime import datetime
+sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
 
-from sqlalchemy.orm import sessionmaker
 
 @pytest.fixture
 def app():
     app = create_app({
         'TESTING': True,
     })
-    
     yield app
 
-@pytest.fixture
-def client(app):
-    return app.test_client()
 
 @pytest.fixture
-def db(app):
+def client(app: Flask):
+    return app.test_client()
+
+
+@pytest.fixture
+def db(app: Flask):
+    
+    load_dotenv()
+    password = os.getenv("DB_PASSWORD")
     db_connect = ""
-    password=""
-    if (app.config['TESTING']):
+    if app.config['TESTING']:
         db_connect = f"postgresql+psycopg://postgres:{password}@127.0.0.1:5432/testing_tutor"
     else:
         db_connect = f"postgresql+psycopg://postgres:{password}@127.0.0.1:5432/prod_tutor"
-    
+
     engine = create_engine(db_connect)
     conn = engine.connect()
     yield conn
-
