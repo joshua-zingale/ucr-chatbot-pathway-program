4dae4a885f6644253ea799a6fd585cd128e72274
Joshua <70401422+joshua-zingale@users.noreply.github.com>
2025-07-08 18:21:23 -0700

    Revert "Added course_documents route skeleton"

--- ucr_chatbot/web_interface/routes.py
+++ ucr_chatbot/web_interface/routes.py
@@ -2,7 +2,7 @@ from flask import (
     Blueprint,
     render_template,
     url_for,
-    redirect,
+    redirect,  # type: ignore
 )
 
 bp = Blueprint("routes", __name__)
@@ -14,39 +14,23 @@ def course_selection():
     return render_template(
         "base.html",
         title="Landing Page",
-        body=f'Select your course. <a href="{url_for(".new_conversation", course_id="1")}"> CS009A </a>',
+        body=f'Select your course. <a href="{url_for(".chat", course_id="1")}"> CS009A </a>',
     )
 
 
-@bp.route("/course/<int:course_id>/chat")
-def new_conversation(course_id: int):
+@bp.route("/chat/<int:course_id>")
+def chat(course_id: int):  # type: ignore
     """Redirects to a page with a new conversation for a course.
     :param course_id: The id of the course for which a conversation will be initialized.
     """
-    return redirect(url_for(".conversation", conversation_id=course_id))
+    return redirect(url_for(".conversation", conversation_id=course_id))  # type: ignore
 
 
-@bp.route("/convsersation/<int:conversation_id>")
+@bp.route("/chat/convsersation/<int:conversation_id>")
 def conversation(conversation_id: int):
-    """Responds with page where a student can interact with a chatbot for a course.
-
-    :param conversation_id: The id of the conversation to be send back to the user.
-    """
+    """Responds with page where a student can interact with a chatbot for a course with <course_id>."""
     return render_template(
         "base.html",
         title="Landing Page",
         body=f"Chat with me about the course for which the conversation with id {conversation_id} exists.",
-    )
-
-
-@bp.route("/course/<int:course_id>/documents")
-def course_documents(course_id: int):
-    """Responds with a page where a course administrator can add more documents
-    to the course for use by the retrieval-augmented generation system.
-    :param course_id: The id of the course for which a conversation will be initialized.
-    """
-    return render_template(
-        "base.html",
-        title="Landing Page",
-        body=f"These are the documents for the course with id {course_id}]",
     )
