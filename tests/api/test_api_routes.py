21f2f82bfbe8ff5ae45db2d5b8ebeb15e08fa649
Joshua Zingale <jtzingale@gmail.com>
2025-07-08 17:35:36 -0700

    Added type hints to tests

--- tests/api/test_api_routes.py
+++ tests/api/test_api_routes.py
@@ -1,3 +1,4 @@
-def test_generate_ok_response(client):
+from flask.testing import FlaskClient
+def test_generate_ok_response(client: FlaskClient):
     response = client.post('/api/generate', json={"prompt": "Four score and seven years ago" , "conversation_id": 0})
     assert "200 OK" == response.status
--- tests/web_interface/test_web_interface_routes.py
+++ tests/web_interface/test_web_interface_routes.py
@@ -1,3 +1,5 @@
-def test_course_selection_ok_response(client):
+from flask.testing import FlaskClient
+
+def test_course_selection_ok_response(client: FlaskClient):
     response = client.get('/')
     assert "200 OK" == response.status
