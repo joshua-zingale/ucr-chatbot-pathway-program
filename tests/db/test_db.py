d2c4b825091bd45b220c62a416fd9ff14da8b02f
Chris <cullm001@ucr.edu>
2025-07-09 14:01:20 -0700

    db and tests

--- tests/db/test_db.py
+++ tests/db/test_db.py
@@ -3,47 +3,65 @@ import os
 from sqlalchemy import insert, select, delete
 from datetime import datetime, timezone
 
-from pgvector.sqlalchemy import Vector
+from sqlalchemy.engine import Connection
+
 
 sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '../../')))
 
 from ucr_chatbot.db.models import *
 
-def test_insert_course(db): #tests insertion into Courses table
-    stmt = insert(Courses).values(id = 100, name = 'CS100')
-    db.execute(stmt)
-    db.commit()
-
-
+def test_add_new_course(db: Connection):
+    """tests the add_new_course wrapper function"""
+    add_new_course(id = 100, name = 'CS100')
     s = select(Courses).where(Courses.id == 100 , Courses.name=='CS100')
     result = db.execute(s)
 
+    answer = None
     for row in result:
         answer = row
-    
+    assert answer is not None
     assert answer == (100,'CS100',)
 
+def test_add_course_integrity(capsys):
+    """tests the add_new_course function exception occurs when error"""
+    add_new_course(id = 100, name = 'CS100')
+    captured = capsys.readouterr()
+    assert "Error adding new course.\n\n" in captured.out
+    
 
 
 
-
-def test_insert_user(db): #tests insertion into User table
-    stmt = insert(Users).values(email = 'test@ucr.edu', first_name = 'John', last_name = 'Doe')
-    db.execute(stmt)
-    db.commit()
-
-
+def test_add_new_user(db: Connection):
+    """tests the add_new_user wrapper function"""
+    add_new_user("test@ucr.edu", "John", "Doe")
     s = select(Users).where(Users.email=='test@ucr.edu', Users.first_name=='John', Users.last_name=='Doe')
     result = db.execute(s)
-    print("enters")
-    print(type(result))
+
+    answer = None
     for row in result:
         answer = row
-    
+    assert answer is not None
     assert answer == ('test@ucr.edu', 'John', 'Doe')
 
-def test_insert_participates(db): #tests insertion into User table
-    print("Running test_insert_participates")
+
+def test_add_user_integrity(capsys):
+    """tests the add_new_user function exception occurs when error"""
+    add_new_user("test@ucr.edu", "John", "Doe")
+    captured = capsys.readouterr()
+    assert "Error adding new user." in captured.out
+
+def test_print_users(capsys):
+    """tests the print_users wrapper function"""
+    print_users()
+    captured = capsys.readouterr()
+    print(repr(captured.out)) 
+    assert captured.out == '+--------------+------+-----+\n| 0            | 1    | 2   |\n|--------------+------+-----|\n| test@ucr.edu | John | Doe |\n+--------------+------+-----+\n'
+
+
+
+def test_insert_participates(db: Connection):
+    """tests if the relationship between a user and a course can be added and selected out of db"""
+
 
     stmt = insert(ParticipatesIn).values(email = 'test@ucr.edu', course_id = 100, role='Student')
     db.execute(stmt)
@@ -52,14 +70,24 @@ def test_insert_participates(db): #tests insertion into User table
 
     s = select(ParticipatesIn).where(ParticipatesIn.email=='test@ucr.edu', ParticipatesIn.course_id==100, ParticipatesIn.role == 'Student')
     result = db.execute(s)
-    print("enters")
-    print(type(result))
+
+    answer = None
     for row in result:
         answer = row
-    
+    assert answer is not None
     assert answer == ('test@ucr.edu', 100, 'Student')
 
-def test_insert_conversations(db): #tests insertion into User table
+def test_print_participation(capsys):
+    """tests the print_participation wrapper function"""
+    print_participation()
+    captured = capsys.readouterr()
+    print(repr(captured.out))  
+    assert captured.out == "+--------------+-----+---------+\n| 0            |   1 | 2       |\n|--------------+-----+---------|\n| test@ucr.edu | 100 | Student |\n+--------------+-----+---------+\n"
+
+
+
+def test_insert_conversations(db: Connection): 
+    """tests if a conversation can be inserted and selected out of db"""
 
 
     stmt = insert(Conversations).values(id =100, initiated_by = 'test@ucr.edu', course_id = 100)
@@ -69,99 +97,137 @@ def test_insert_conversations(db): #tests insertion into User table
 
     s = select(Conversations).where(Conversations.id ==100, Conversations.initiated_by == 'test@ucr.edu', Conversations.course_id == 100)
     result = db.execute(s)
-    print("enters")
-    print(type(result))
+
+    answer = None
     for row in result:
         answer = row
-    
+    assert answer is not None
     assert answer == (100, 'test@ucr.edu',100)
 
-def test_insert_messages(db): #tests insertion into User table
-    curr_time = datetime.now(timezone.utc).replace(tzinfo=None)
-    stmt = insert(Messages).values(id = 100, body = "testing", timestamp = curr_time, type="StudentMessages", conversation_id = 100, written_by = 'test@ucr.edu')
+def test_insert_messages(db: Connection): 
+    """tests if a message can be inserted and selected out of db"""
+    
+    curr_time = datetime.now(timezone.utc)
+    stmt = insert(Messages).values(id = 100, body = "testing", timestamp = curr_time, type=MessageType.STUDENT_MESSAGES, conversation_id = 100, written_by = 'test@ucr.edu')
     db.execute(stmt)
     db.commit()
 
 
-    s = select(Messages).where(Messages.id == 100,Messages.body == "testing", Messages.type=="StudentMessages", Messages.conversation_id == 100, Messages.written_by == 'test@ucr.edu')
+    s = select(Messages).where(Messages.id == 100,Messages.body == "testing", Messages.type==MessageType.STUDENT_MESSAGES, Messages.conversation_id == 100, Messages.written_by == 'test@ucr.edu')
     result = db.execute(s)
-    print("enters")
-    print(type(result))
+
+    answer = None
     for row in result:
         answer = row
-    
-    assert answer ==(100, 'testing', curr_time, 'StudentMessages', 100, 'test@ucr.edu')
+    assert answer is not None
+    assert answer[0] ==(100)
 
-def test_insert_documents(db): #tests insertion into User table
-    stmt = insert(Documents).values(file_path="slide_1.pdf", course_id=100)
-    db.execute(stmt)
-    db.commit()
-
-
+def test_add_new_document(db: Connection):
+    """tests the add_new_documents wrapper function"""
+    add_new_document(file_path="slide_1.pdf", course_id=100)
     s = select(Documents).where(Documents.file_path=="slide_1.pdf", Documents.course_id==100)
     result = db.execute(s)
-    print("enters")
-    print(type(result))
+
+    answer = None
     for row in result:
         answer = row
-    
-    assert answer ==("slide_1.pdf", 100)
+    assert answer is not None
+    assert answer == ("slide_1.pdf", 100)
 
-def test_insert_segments(db): #tests insertion into User table
-    stmt = insert(Segments).values(id =100, text = "hello", document="slide_1.pdf")
+def test_add_document_integrity(capsys):
+    """tests the add_new_document function exception occurs when error"""
+    add_new_document(file_path="slide_1.pdf", course_id=100)
+    captured = capsys.readouterr()
+    assert "Error adding document. Course id must exist.\n\n" in captured.out
+
+def test_insert_segments(db: Connection): 
+    """tests if a segment can be inserted and selected out of db"""
+    stmt = insert(Segments).values(id =100, text = "hello", document_id="slide_1.pdf")
     db.execute(stmt)
     db.commit()
 
 
-    s = select(Segments).where(Segments.id==100, Segments.text=="hello", Segments.document=="slide_1.pdf")
+    s = select(Segments).where(Segments.id==100, Segments.text=="hello", Segments.document_id=="slide_1.pdf")
     result = db.execute(s)
-    print("enters")
-    print(type(result))
+
+    answer = None
     for row in result:
         answer = row
-    
+    assert answer is not None
     assert answer ==(100,"hello", "slide_1.pdf")
 
-def test_insert_embeddings(db): #tests insertion into User table
-    emb = ([0.,0.,0.,0.,0.,0.,0.,0.,0.,0.])
+def test_insert_embeddings(db: Connection): 
+    """tests if an embedding can be inserted and selected out of db"""
+    emb = [0,0,0,0.,0,0,0,0,0,0]
     stmt = insert(Embeddings).values(id =100, vector=emb, segment_id=100)
     db.execute(stmt)
     db.commit()
 
 
-    s = select(Embeddings).where(Embeddings.id ==100, Embeddings.vector==emb, Embeddings.segment_id==100)
+    s = select(Embeddings).where(Embeddings.id ==100)
     result = db.execute(s)
-    print("enters")
-    print(type(result))
+
+    answer = None
     for row in result:
         answer = row
-    
-    assert answer ==(100,emb, 100)
+    assert answer is not None
+    assert answer[0] ==(100)
 
-def test_delete_embeddings(db): #tests deleting from User table
+def test_insert_reference(db: Connection): 
+    """tests if a reference between a message and segment can be inserted and selected out of db"""
+    stmt = insert(References).values(message =100, segment=100)
+    db.execute(stmt)
+    db.commit()
+
+
+    s = select(References).where(References.message==100, References.segment==100)
+    result = db.execute(s)
+
+    answer = None
+    for row in result:
+        answer = row
+    assert answer is not None
+    assert answer == (100,100)
+
+def test_delete_reference(db: Connection): 
+    """tests if a reference between a message and segment can be deleted out of db"""
+    stmt = delete(References).where(References.message==100, References.segment==100)
+    db.execute(stmt)
+    db.commit()
+
+    s = select(References).where(References.message==100, References.segment==100)
+    result = db.execute(s)
+    result = list(result)
+   
+    assert len(result)==0
+
+def test_delete_embeddings(db: Connection): 
+    """tests if an embedding can be deleted from db"""
     stmt = delete(Embeddings).where(Embeddings.id ==100, Embeddings.vector==[0,0,0,0,0,0,0,0,0,0], Embeddings.segment_id==100)
     db.execute(stmt)
     db.commit()
 
-    s = select(Segments).where(Embeddings.id ==100, Embeddings.vector==[0,0,0,0,0,0,0,0,0,0], Embeddings.segment_id==100)
+    s = select(Embeddings).where(Embeddings.id ==100)
     result = db.execute(s)
     result = list(result)
    
     assert len(result)==0
 
-def test_delete_segments(db): #tests deleting from User table
-    stmt = delete(Segments).where(Segments.id==100, Segments.text=="hello", Segments.document=="slide_1.pdf")
+def test_delete_segments(db: Connection):
+    """tests if a segment can be deleted from db"""
+    stmt = delete(Segments).where(Segments.id==100, Segments.text=="hello", Segments.document_id=="slide_1.pdf")
     db.execute(stmt)
     db.commit()
 
-    s = select(Segments).where(Segments.id==100, Segments.text=="hello", Segments.document=="slide_1.pdf")
+    s = select(Segments).where(Segments.id==100, Segments.text=="hello", Segments.document_id=="slide_1.pdf")
     result = db.execute(s)
     result = list(result)
    
     assert len(result)==0
 
 
-def test_delete_documents(db): #tests deleting from User table
+def test_delete_documents(db: Connection):
+    """tests if a document can be deleted from db"""
     stmt = delete(Documents).where(Documents.file_path=="slide_1.pdf", Documents.course_id==100)
     db.execute(stmt)
     db.commit()
@@ -174,18 +240,20 @@ def test_delete_documents(db): #tests deleting from User table
 
 
 
-def test_delete_messages(db): #tests deleting from User table
-    stmt = delete(Messages).where(Messages.id == 100,Messages.body == "testing", Messages.type=="StudentMessages", Messages.conversation_id == 100, Messages.written_by == 'test@ucr.edu')
+def test_delete_messages(db: Connection):
+    """tests if a message can be deleted from db"""
+    stmt = delete(Messages).where(Messages.id == 100,Messages.body == "testing", Messages.type==MessageType.STUDENT_MESSAGES, Messages.conversation_id == 100, Messages.written_by == 'test@ucr.edu')
     db.execute(stmt)
     db.commit()
 
-    s = select(Messages).where(Messages.id == 100,Messages.body == "testing", Messages.type=="StudentMessages", Messages.conversation_id == 100, Messages.written_by == 'test@ucr.edu')
+    s = select(Messages).where(Messages.id == 100,Messages.body == "testing", Messages.type==MessageType.STUDENT_MESSAGES, Messages.conversation_id == 100, Messages.written_by == 'test@ucr.edu')
     result = db.execute(s)
     result = list(result)
    
     assert len(result)==0
 
-def test_delete_conversations(db): #tests deleting from User table
+def test_delete_conversations(db: Connection):
+    """tests if a conversation can be deleted from db"""
     stmt = delete(Conversations).where(Conversations.id ==100, Conversations.initiated_by == 'test@ucr.edu', Conversations.course_id == 100)
     db.execute(stmt)
     db.commit()
@@ -197,7 +265,8 @@ def test_delete_conversations(db): #tests deleting from User table
     assert len(result)==0
 
 
-def test_delete_participates(db): #tests deleting from User table
+def test_delete_participates(db: Connection):
+    """tests if a user course relationship can be deleted from db"""
     stmt = delete(ParticipatesIn).where(ParticipatesIn.email=='test@ucr.edu', ParticipatesIn.course_id == 100)
     db.execute(stmt)
     db.commit()
@@ -209,7 +278,8 @@ def test_delete_participates(db): #tests deleting from User table
     assert len(result)==0
 
 
-def test_delete_course(db): #tests deleting from Courses table
+def test_delete_course(db: Connection):
+    """tests if a course can be deleted from db"""
     stmt = delete(Courses).where(Courses.id==100)
     db.execute(stmt)
     db.commit()
@@ -221,7 +291,8 @@ def test_delete_course(db): #tests deleting from Courses table
     
     assert len(result)==0
 
-def test_delete_user(db): #tests deleting from User tabl
+def test_delete_user(db: Connection): 
+    """tests if a user can be deleted from db"""
     stmt = delete(Users).where(Users.email=='test@ucr.edu', Users.first_name=='John', Users.last_name=='Doe')
     db.execute(stmt)
     db.commit()
@@ -231,6 +302,8 @@ def test_delete_user(db): #tests deleting from User tabl
     result = list(result)
    
     assert len(result)==0
+
+
 
 
 
--- ucr_chatbot/db/models.py
+++ ucr_chatbot/db/models.py
@@ -1,78 +1,145 @@
-from sqlalchemy import create_engine, String, Column, Integer, DateTime, ForeignKey, Text
-from sqlalchemy.orm import declarative_base, mapped_column,  relationship
-from datetime import datetime
+from sqlalchemy import (
+    create_engine,
+    String,
+    Column,
+    Integer,
+    DateTime,
+    ForeignKey,
+    Text,
+    Enum,
+)
+from sqlalchemy.orm import declarative_base, mapped_column, relationship, Session
+import enum
+from pgvector.sqlalchemy import Vector  # type: ignore
+from datetime import datetime, timezone
+from dotenv import load_dotenv
+import os
+from sqlalchemy.exc import IntegrityError
 
-from sqlalchemy.orm import sessionmaker
-from pgvector.sqlalchemy import Vector
-password = ""
-engine = create_engine(f"postgresql+psycopg://postgres:{password}@127.0.0.1:5432/testing_tutor")
+from tabulate import tabulate
 
+load_dotenv()
+
+
+password = os.getenv("DB_PASSWORD")
+
+engine = create_engine(
+    f"postgresql+psycopg://postgres:{password}@127.0.0.1:5432/testing_tutor"
+)
 
 base = declarative_base()
+
+
+class MessageType(enum.Enum):
+    """Manditory choices for Message type"""
+
+    ASSISTANT_MESSAGES = "AssistantMessage"
+    STUDENT_MESSAGES = "StudentMessage"
+    BOT_MESSAGES = "BotMessage"
+
+
 class Users(base):
+    """Represents a User and their profile information"""
+
     __tablename__ = "Users"
-    email = Column(String,primary_key=True)
+    email = Column(String, primary_key=True)
     first_name = Column(String)
     last_name = Column(String)
 
-    # conversations = relationship('Conversations', back_populates='Users', uselist=True)
-    # messages = relationship('Messages', back_populates='Users', uselist=True)
+    conversations = relationship("Conversations", back_populates="user", uselist=True)
+    messages = relationship("Messages", back_populates="user", uselist=True)
+    participates = relationship("ParticipatesIn", back_populates="user")
+
 
 class ParticipatesIn(base):
+    """Represents the enrollment between users and courses"""
+
     __tablename__ = "ParticipatesIn"
     email = Column(String, ForeignKey("Users.email"), primary_key=True)
     course_id = Column(Integer, ForeignKey("Courses.id"), primary_key=True)
-    role = Column(String, nullable = False)
+    role = Column(String, nullable=False)
+
+    user = relationship("Users", back_populates="participates")
+    course = relationship("Courses", back_populates="participates")
+
 
 class Conversations(base):
+    """Represents the conversations a user can initiate"""
+
     __tablename__ = "Conversations"
     id = Column(Integer, primary_key=True, autoincrement=True)
     initiated_by = Column(String, ForeignKey("Users.email"), nullable=False)
     course_id = Column(Integer, ForeignKey("Courses.id"), nullable=False)
 
-    # course = relationship('Courses', back_populates='Conversations')
-    # messages = relationship('Messages', back_populates='Conversations', uselist = True)
-    # user = relationship('Users', back_populates='Conversations')
+    course = relationship("Courses", back_populates="conversations")
+    messages = relationship("Messages", back_populates="conversation", uselist=True)
+    user = relationship("Users", back_populates="conversations")
+
 
 class Courses(base):
+    """Represents a course"""
+
     __tablename__ = "Courses"
     id = Column(Integer, primary_key=True, autoincrement=True)
     name = Column(String)
 
-    # conversations = relationship('Conversations', back_populates='Courses',uselist=True)
-    # documents = relationship('Documents', back_populates='Courses', uselist=True)
+    conversations = relationship("Conversations", back_populates="course", uselist=True)
+    documents = relationship("Documents", back_populates="course", uselist=True)
+    participates = relationship("ParticipatesIn", back_populates="course")
+
+
 class Documents(base):
+    """Represents a stored file to be references with queries"""
+
     __tablename__ = "Documents"
     file_path = Column(String, primary_key=True)
     course_id = Column(Integer, ForeignKey("Courses.id"), nullable=False)
 
-    # course = relationship('Course', back_populates='Documents')
+    course = relationship("Courses", back_populates="documents")
+    segments = relationship("Segments", back_populates="document", uselist=True)
+
 
 class Segments(base):
+    """Represents a section of a document to be embedded"""
+
     __tablename__ = "Segments"
     id = Column(Integer, primary_key=True, autoincrement=True)
     text = Column(String)
-    document = Column(String, ForeignKey("Documents.file_path"), nullable=False)
+    document_id = Column(String, ForeignKey("Documents.file_path"), nullable=False)
+
+    document = relationship("Documents", back_populates="segments")
+    embeddings = relationship("Embeddings", back_populates="segment", uselist=True)
+
 
 class Messages(base):
+    """Represents a specific message between a user and LLM"""
+
     __tablename__ = "Messages"
     id = Column(Integer, primary_key=True, autoincrement=True)
     body = Column(Text)
-    timestamp = Column(DateTime, default=datetime.utcnow)
-    type = Column(String) #must be of 3 types num types
+    timestamp = Column(DateTime, default=lambda: datetime.now(timezone.utc))
+    type = Column(Enum(MessageType))
     conversation_id = Column(Integer, ForeignKey("Conversations.id"), nullable=False)
-    written_by = Column(String, ForeignKey("Users.email"), nullable = False)
+    written_by = Column(String, ForeignKey("Users.email"), nullable=False)
 
-    # conversation = relationship('Conversation', back_populates='Messages')
-    # user = relationship('Users', back_populates='Messages')
+    conversation = relationship("Conversations", back_populates="messages")
+    user = relationship("Users", back_populates="messages")
+
 
 class Embeddings(base):
+    """Represents the embedding of a segment"""
+
     __tablename__ = "Embeddings"
     id = Column(Integer, primary_key=True, autoincrement=True)
-    vector = mapped_column(Vector(10)) #10 just for initial testing
+    vector = mapped_column(Vector)
     segment_id = Column(Integer, ForeignKey("Segments.id"), nullable=False)
 
+    segment = relationship("Segments", back_populates="embeddings")
+
+
 class References(base):
+    """Represents the relationship between a message and referenced segments"""
+
     __tablename__ = "References"
     message = Column(Integer, ForeignKey("Messages.id"), primary_key=True)
     segment = Column(Integer, ForeignKey("Segments.id"), primary_key=True)
@@ -82,5 +149,73 @@ base.metadata.drop_all(engine)
 base.metadata.create_all(engine)
 
 
-#dont cascade  leave as normal
-#create script that takes in emails, create dummy password, put into db
+def add_new_user(email: str, first_name: str, last_name: str):
+    """Adds new user entry to Users table with the given parameters."""
+    with Session(engine) as session:
+        try:
+            new_user = Users(email=email, first_name=first_name, last_name=last_name)
+
+            session.add_all([new_user])
+            session.commit()
+            print("New user added successfully.\n")
+        except IntegrityError:
+            session.rollback()
+            print("Error adding new user.\n")
+
+
+def add_new_course(id: int, name: str):
+    """Adds new course to the Courses table with the given parameters."""
+    with Session(engine) as session:
+        try:
+            new_course = Courses(
+                id=id,
+                name=name,
+            )
+
+            session.add(new_course)
+            session.commit()
+            print("New course added successfully.\n")
+        except IntegrityError:
+            session.rollback()
+            print("Error adding new course.\n")
+
+
+def add_new_document(file_path: str, course_id: int):
+    """Adds new document to the Documents table with the given parameters."""
+    with Session(engine) as session:
+        try:
+            new_document = Documents(
+                file_path=file_path,
+                course_id=course_id,
+            )
+            session.add(new_document)
+            session.commit()
+            print("Document added successfully.\n")
+        except IntegrityError:
+            session.rollback()
+            print("Error adding document. Course id must exist.\n")
+
+
+def print_users():
+    """prints all users and their information"""
+    with Session(engine) as session:
+        all_entries = session.query(Users).all()
+        rows: list[typing.Tuple[Column[str], Column[str], Column[str]]] = []
+
+        for row in all_entries:
+            rows.append((row.email, row.first_name, row.last_name))
+        print(tabulate(rows, headers="keys", tablefmt="psql"))
+
+
+import typing
+
+
+def print_participation():
+    """prints all relationships between users and courses"""
+    with Session(engine) as session:
+        all_entries = session.query(ParticipatesIn).all()
+        rows: list[typing.Tuple[Column[str], Column[int], Column[str]]] = []
+
+        for row in all_entries:
+            rows.append((row.email, row.course_id, row.role))
+        print(tabulate(rows, headers="keys", tablefmt="psql"))
