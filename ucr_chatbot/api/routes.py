defdbedf18131efff384420e58301f11c8f61f09
Gianna <gpnicomedes@gmail.com>
2025-07-11 13:40:39 -0700

    Landing page UI

--- ucr_chatbot/api/routes.py
+++ ucr_chatbot/api/routes.py
@@ -1,9 +1,63 @@
-from flask import Blueprint, request, jsonify, Response
+from flask import (
+    Blueprint, 
+    request, 
+    jsonify, 
+    Response,
+    render_template,
+    url_for,
+    redirect,
+)
 from .context_retrieval import retriever
 from .language_model.response import client as client
 import json
 
 bp = Blueprint("routes", __name__)
+
+@bp.route("/")
+def course_selection():
+    """Responds with a landing page where a student can select a course"""
+    return render_template(
+        "landing_page.html",
+        title="Landing Page",
+        body=f'Select your course. <a href="{url_for(".new_conversation", course_id="1")}"> CS009A </a>',
+    )
+
+
+@bp.route("/course/<int:course_id>/chat")
+def new_conversation(course_id: int):
+    """Redirects to a page with a new conversation for a course.
+    :param course_id: The id of the course for which a conversation will be initialized.
+    """
+    return redirect(url_for(".conversation", conversation_id=course_id))
+
+
+@bp.route("/convsersation/<int:conversation_id>")
+def conversation(conversation_id: int):
+    """Responds with page where a student can interact with a chatbot for a course.
+
+    :param conversation_id: The id of the conversation to be send back to the user.
+    """
+    return render_template(
+        "base.html",
+        title="Landing Page",
+        body=f"Chat with me about the course for which the conversation with id {conversation_id} exists.",
+    )
+
+# NOT RESPONSIBLE FOR THIS PART =================================================================================================
+
+@bp.route("/course/<int:course_id>/documents")
+def course_documents(course_id: int):
+    """Responds with a page where a course administrator can add more documents
+    to the course for use by the retrieval-augmented generation system.
+    :param course_id: The id of the course for which a conversation will be initialized.
+    """
+    return render_template(
+        "base.html",
+        title="Landing Page",
+        body=f"These are the documents for the course with id {course_id}",
+    )
+
+# STUFF FROM SPRINT 1 ===========================================================================================================
 
 SYSTEM_PROMPT = """# Main directive
 You are a helpful student tutor for a university computer science course. You must assist students in their learning by answering question in a didactically useful way. You should only answer questions if you are certain that you know the correct answer.
@@ -17,7 +71,6 @@ If the context is not relevant, than you should tell the student, "I cannot find
 ## Question
 {question}
 """
-
 
 @bp.route("/generate", methods=["POST"])
 def generate():
