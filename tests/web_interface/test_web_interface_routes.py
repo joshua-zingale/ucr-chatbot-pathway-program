93667cf03e76653f94732ef92ba6e5e8f155323d
Chris <cullm001@ucr.edu>
2025-07-17 17:30:27 -0700

    fixed routing

--- tests/web_interface/test_web_interface_routes.py
+++ tests/web_interface/test_web_interface_routes.py
@@ -8,10 +8,10 @@ from unittest.mock import MagicMock
 def test_course_selection_ok_response(client: FlaskClient):
     response = client.get('/')
     assert "200 OK" == response.status
-    assert b"<h1>SELECT A COURSE</h1>" in response.data
+    assert b"<h1>ScotGPT</h1>" in response.data
 
 def test_new_conversation_ok_response(client: FlaskClient):
-    response = client.get('/new_conversation/10/chat')
+    response = client.get('/conversation/new/10/chat')
     assert "200 OK" == response.status
     assert b"<h2>CONVERSATIONS</h2>" in response.data
 
