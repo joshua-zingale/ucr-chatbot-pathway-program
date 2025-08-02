26db6124b67c0dff61b37412149bbf0329d8a8ee
AgentWombat <jtzingale@gmail.com>
2025-08-01 17:59:17 -0700

    Moved DB constants to config

--- ucr_chatbot/__init__.py
+++ ucr_chatbot/__init__.py
@@ -12,7 +12,7 @@ from authlib.integrations.flask_client import OAuth  # type: ignore
 from flask_login import LoginManager  # type: ignore
 from ucr_chatbot.db.models import Users, Session, engine
 from ucr_chatbot.web_interface.routes import bp
-from dotenv import load_dotenv
+from .config import Config
 
 
 def create_app(test_config: Mapping[str, Any] | None = None):
@@ -22,20 +22,14 @@ def create_app(test_config: Mapping[str, Any] | None = None):
     :return: The Flask application.
     """
 
-    load_dotenv()
 
     app = Flask(__name__, instance_relative_config=True)
-    app.secret_key = os.environ.get("SECRET_KEY", "fallback-dev-secret")
     app.debug = False
 
-    if test_config is None:
-        # load the instance config, if it exists, when not testing
-        app.config.from_pyfile("config.py", silent=True)
-    else:
-        # load the test config if passed in
+    app.config.from_object(Config)
+    if test_config:
         app.config.from_mapping(test_config)
 
-    # ensure the instance folder exists
     instance_path = Path(app.instance_path)
     if not instance_path.is_dir():
         instance_path.mkdir(parents=True, exist_ok=True)
