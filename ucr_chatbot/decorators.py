e72598c2649c0e5d26471db6083efe1d2e275352
Connor <ccclaborn22@gmail.com>
2025-08-07 13:17:09 -0700

    Updated chat response for comments

--- ucr_chatbot/decorators.py
+++ ucr_chatbot/decorators.py
@@ -9,7 +9,7 @@ from ucr_chatbot.db.models import (
 )
 from typing import Callable, ParamSpec, Optional, cast
 from flask.typing import ResponseReturnValue  # type: ignore
-import os
+# import os
 
 P = ParamSpec("P")  # preserves decorated function's param types
 
@@ -40,7 +40,6 @@ def roles_required(
                     Optional[str], kwargs.get("file_path")
                 ) or request.args.get("file_path")
                 if file_path:
-                    file_path = file_path.replace(os.sep, "/")
                     with DBSession(engine) as db:
                         doc = db.query(Documents).filter_by(file_path=file_path).first()
                         if doc:
