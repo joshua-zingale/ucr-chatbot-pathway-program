0c3c63697e529a8b785b87ee07434a928df4cdb8
Emily Clark <emilyclark@gmail.com>
2025-08-08 07:58:42 -0700

    Added tests for cli parser

---
+++ tests/db/test_cli.py
@@ -0,0 +1,51 @@
+import sys
+import shlex
+import pytest
+from pathlib import Path
+from sqlalchemy import insert, select, delete, inspect
+from sqlalchemy.engine import Connection
+
+
+from ucr_chatbot.db.models import *
+from ucr_chatbot.db.cli import *
+from helper_functions import clear_db
+
+
+
+def test_main(capsys):
+  base.metadata.drop_all(engine)
+  main(shlex.split('initialize'))
+  output = capsys.readouterr().out.rstrip()
+  assert output == "Database initialized."
+
+def test_initialize():
+  base.metadata.drop_all(engine)
+  initialize(False)
+  inspector = inspect(engine)
+  assert inspector.has_table("Users") == True
+
+def test_initialize_force():
+  base.metadata.drop_all(engine)
+  initialize(False)
+  initialize(True)
+  inspector = inspect(engine)
+  assert inspector.has_table("Users") == True
+
+def test_mock(db: Connection):
+  initialize(True)
+  mock()
+  s = select(Courses).where(Courses.id==1)
+  result = db.execute(s)
+
+  answer = None
+  for row in result:
+    answer = row
+  assert answer.name == 'CS010C'
+
+  s = select(Users).where(Users.email=='test001@ucr.edu')
+  result = db.execute(s)
+
+  answer = None
+  for row in result:
+    answer = row
+  assert answer.email == 'test001@ucr.edu'
